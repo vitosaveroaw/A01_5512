101d1df28fca99e89389e274490154be60d4e3b7
vitosaveroaw <vitosaveroaw@gmail.com>
2026-09-06 23:07:56 -0400

    A01 assignment part 1-5
    Installing requirements need tweaking

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Data: California Housing dataset
 
 ## How to run the script
+Python version I use: 3.14.5
+
+pandas changed to 
+
+pip install -r requirements.txt
 
 ## Expected Output
---
+++ src/boxplot.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+
+# Load California Housing dataset
+housing = fetch_california_housing(as_frame=True)
+
+# Features + target as a single DataFrame
+df = housing.frame
+
+# Quick check
+print(df.head())
+print(df.shape)
+
+# you can save the boxplot...
