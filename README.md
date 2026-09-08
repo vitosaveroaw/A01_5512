434f7161f729b59106993d41b0d57adee65d0a8e
vitosaveroaw <vitosaveroaw@gmail.com>
2026-09-07 20:14:59 -0400

    Finished part 5
    pandas version change to newer version due to incompatibility with my python version

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # A01 Assignment
-
 Data: California Housing dataset
+Python version: 3.14.5
 
 ## How to run the script
-Python version I use: 3.14.5
+cd path/tp/A01_5512
 
-pandas changed to 
-
+On your folder's terminal run the command below one by one
 pip install -r requirements.txt
+python src/boxplot.py
 
 ## Expected Output
+After you run the boxplot.py you will have save the plot from the code into figs/boxplot.png
+![boxplot of MedHouseVal](figs/boxplot.png)
--- src/boxplot.py
+++ src/boxplot.py
@@ -11,4 +11,17 @@ df = housing.frame
 print(df.head())
 print(df.shape)
 
-# you can save the boxplot...
+# Create boxplot and save it as a PNG file
+import matplotlib.pyplot as plt
+
+# Create a boxplot for 'MedHouseVal'
+plt.figure(figsize=(8, 6))
+plt.boxplot(df['MedHouseVal'])
+plt.title('Boxplot of Median House Value')
+plt.ylabel('Median House Value')
+plt.xticks([]) # Hide x-axis ticks for a single boxplot
+# plt.ylim(4.5, 5.1) # Zooming to the 
+
+# Save the boxplot
+plt.savefig('figs/boxplot.png', bbox_inches='tight', dpi=300)
+print("Boxplot saved as 'figs/boxplot.png'")
